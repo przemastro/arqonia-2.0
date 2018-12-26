81bea0d9ece9f6f2c2ca1fe539d333d9cb2ec3ff
Przemek <przemastro@gmail.com>
2018-12-26 18:45:12 +0100

    Added observation area: modals, buttons

---
+++ src/main/webapp/src/app/remove-observation-modal/remove-observation-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-observation-modal',
+  templateUrl: './remove-observation-modal.component.html',
+  styleUrls: ['./remove-observation-modal.component.css']
+})
+export class RemoveObservationModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
