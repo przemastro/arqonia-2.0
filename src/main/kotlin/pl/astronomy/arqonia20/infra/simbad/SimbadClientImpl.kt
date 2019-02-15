7839dcac76e047f3b52fbae046e26efa9ac94bc8
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-02-16 00:00:07 +0100

    Simbad client - > added initial formData form request

--- src/main/kotlin/pl/astronomy/arqonia20/infra/simbad/SimbadClientImpl.kt
+++ src/main/kotlin/pl/astronomy/arqonia20/infra/simbad/SimbadClientImpl.kt
@@ -3,8 +3,11 @@ package pl.astronomy.arqonia20.infra.simbad
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.util.MultiValueMap
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import pl.astronomy.arqonia20.domain.search.SimbadClient
 import reactor.core.publisher.Mono
@@ -20,9 +23,19 @@ class SimbadClientImpl(
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build()
 
-    // TODO Finish implementation !
-    override fun getAllIdentifiers(objectName: String): Mono<AllIdentifiers> =
-            Mono.empty()
+    override fun getAllIdentifiers(objectName: String): Mono<AllIdentifiers> {
+        lateinit var formData: MultiValueMap<String, String>
+        // TODO All rest of form data ! (see PostMan) !
+        formData.add("request", "doQuery")
+
+        return client.method(HttpMethod.POST)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept( MediaType.APPLICATION_JSON )
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve()
+                .bodyToMono(AllIdentifiers::class.java)
+    }
+
 }
 
 // TODO Move to another package this data class !
