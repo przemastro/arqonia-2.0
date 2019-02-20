3355bb9960991c0f1b072dc23a62ad5d31e01448
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-02-20 20:53:58 +0100

    Added config data 'selected catalogs' -> on this base, will be created response from Vizier client

--- src/main/kotlin/pl/astronomy/arqonia20/domain/search/SearchService.kt
+++ src/main/kotlin/pl/astronomy/arqonia20/domain/search/SearchService.kt
@@ -1,5 +1,6 @@
 package pl.astronomy.arqonia20.domain.search
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import pl.astronomy.arqonia20.logger
 import reactor.core.publisher.Mono
@@ -7,13 +8,19 @@ import reactor.core.publisher.Mono
 @Service
 class SearchService(
         private val simbadClient: SimbadClient,
-        private val vizierClient: VizierClient
+        private val vizierClient: VizierClient,
+        @Value("\${simbadClient.selectedCatalogs}") private val selectedCatalogs: List<String>
 ) {
     fun searchByType(objectName: String, objectType: String): Mono<*> {
         logger.info("vizier checking...")
-        return vizierClient.getObjectDetailsByCatalog("I/239/hip_main", "21421")
-
+        // 1. Get all identifiers from Simbad
 //        return simbadClient.getAllIdentifiers(objectName)
+
+        // 2. Filter all identifiers only for chosen one:
+
+
+        // 3. Get details for all filtered identifiers
+        return vizierClient.getObjectDetailsByCatalog("I/239/hip_main", "21421")
     }
 
     companion object {
