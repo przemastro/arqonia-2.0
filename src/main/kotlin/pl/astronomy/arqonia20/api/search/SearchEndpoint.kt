1c3bca9f5d49bd71c973481eba87feebd7e615ef
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-02-24 22:02:18 +0100

    Data for stars now are returned from VizieR - not from mock

--- src/main/kotlin/pl/astronomy/arqonia20/api/search/SearchEndpoint.kt
+++ src/main/kotlin/pl/astronomy/arqonia20/api/search/SearchEndpoint.kt
@@ -20,8 +20,7 @@ class SearchEndpoint(
     fun searchV2(
             @RequestParam(required = true) objectName: String,
             @RequestParam(required = false, defaultValue = "star") objectType: String): Mono<*> {
-//            @RequestParam(required = false, defaultValue = "star") objectType: String): Flux<*> {
-        logger.info("Searching user credentials...")
+        logger.info("Searching object type '$objectType', with name '$objectName'...")
 
         return searchService
                 .searchByType(objectName, objectType)
@@ -33,18 +32,21 @@ class SearchEndpoint(
     fun search(
             @RequestParam(required = true) objectName: String,
             @RequestParam(required = false, defaultValue = "star") objectType: String): Mono<*> {
-        logger.info("Searching user credentials...")
+        logger.info("Searching object type '$objectType', with name '$objectName'...")
 
-        return Mono.just(
-                listOf(
-                        StarObject("SAO", "Aldebaran", "23h 0m 0", "+34 4 4", "3", "4.5", "3.3", "BV", "UB", "RI", "VI", "A0"),
-                        StarObject("HIP", "12323", "23h 0m 0", "+35 5 4", "3.1", "4.1", "3.34", "BV", "UB", "RI", "VI", "A1"),
-                        StarObject("TYC2", "345435", "23h 0m 0", "+35 5 4", "3.6", "4.4", "3.34", "BV", "UB", "RI", "VI", "A1"),
-                        StarObject("HD", "153453", "23h 0m 0", "+35 5 4", "3.3", "4.3", "3.34", "BV", "UB", "RI", "VI", "A1"),
-                        StarObject("HR", "123455", "23h 0m 0", "+35 5 4", "3.1", "4.4", "3.4", "BV", "UB", "RI", "VI", "A1")
+        return searchService
+                .searchByType(objectName, objectType)
 
-                )
-        )
+//        return Mono.just(
+//                listOf(
+//                        StarObject("SAO", "Aldebaran", "23h 0m 0", "+34 4 4", "3", "4.5", "3.3", "BV", "UB", "RI", "VI", "A0"),
+//                        StarObject("HIP", "12323", "23h 0m 0", "+35 5 4", "3.1", "4.1", "3.34", "BV", "UB", "RI", "VI", "A1"),
+//                        StarObject("TYC2", "345435", "23h 0m 0", "+35 5 4", "3.6", "4.4", "3.34", "BV", "UB", "RI", "VI", "A1"),
+//                        StarObject("HD", "153453", "23h 0m 0", "+35 5 4", "3.3", "4.3", "3.34", "BV", "UB", "RI", "VI", "A1"),
+//                        StarObject("HR", "123455", "23h 0m 0", "+35 5 4", "3.1", "4.4", "3.4", "BV", "UB", "RI", "VI", "A1")
+//
+//                )
+//        )
     }
 
     companion object {
