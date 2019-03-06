bdd38ddb8395233cc6dbbdb3d83036dd34430b5c
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-03-06 22:29:26 +0100

    Testing version of 'flux' search for stars searching -> added

--- src/main/kotlin/pl/astronomy/arqonia20/api/search/SearchEndpoint.kt
+++ src/main/kotlin/pl/astronomy/arqonia20/api/search/SearchEndpoint.kt
@@ -6,6 +6,7 @@ import pl.astronomy.arqonia20.domain.comet.CometService
 import pl.astronomy.arqonia20.domain.search.ObjectType
 import pl.astronomy.arqonia20.domain.search.ObjectType.STAR
 import pl.astronomy.arqonia20.domain.search.SearchService
+import pl.astronomy.arqonia20.domain.star.StarService
 import pl.astronomy.arqonia20.logger
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -15,7 +16,8 @@ import reactor.core.publisher.Mono
 @RequestMapping
 class SearchEndpoint(
         private val searchService: SearchService,
-        private val cometService: CometService
+        private val cometService: CometService,
+        private val starService: StarService
 ) {
 
     @PostMapping("/search")
@@ -42,7 +44,9 @@ class SearchEndpoint(
             @RequestParam(required = false, defaultValue = "star") objectType: String): Flux<*> {
         logger.info("Searching object type '$objectType', with name '$objectName'...")
 
-        return cometService.getCometData(objectName)
+//        return cometService.getCometData(objectName)
+
+        return starService.getStarsDataV2(objectName)
     }
 
     companion object {
--- src/main/kotlin/pl/astronomy/arqonia20/domain/star/StarService.kt
+++ src/main/kotlin/pl/astronomy/arqonia20/domain/star/StarService.kt
@@ -3,6 +3,7 @@ package pl.astronomy.arqonia20.domain.star
 import org.springframework.stereotype.Service
 import pl.astronomy.arqonia20.domain.star.SelectedCatalogsEnum.*
 import pl.astronomy.arqonia20.logger
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Service
@@ -11,6 +12,36 @@ class StarService(
         private val vizierClient: VizierClient,
         private val vizierQueries: VizierQueriesConfig
 ) {
+
+    fun getStarsDataV2(objectName: String) =
+            simbadClient.getAllIdentifiers(objectName)
+                    .flatMap { ids ->
+                        Mono.fromCallable {
+                            ids.data
+                                    .flatten()
+                                    .filter { vizierQueries.queries.keys.contains(it.substringBefore(" ")) }
+                        }
+                    }
+                    .flatMapMany {ids ->
+                        Flux.zip(
+                                vizierClient.getObjectDetails(vizierQueries.queries.getValue(SAO.name), extractRawId(ids, SAO.name)),
+                                vizierClient.getObjectDetails(vizierQueries.queries.getValue(HIP.name), extractRawId(ids, HIP.name)),
+                                vizierClient.getObjectDetails(vizierQueries.queries.getValue(TYC.name), extractRawId(ids, TYC.name)),
+                                vizierClient.getObjectDetails(vizierQueries.queries.getValue(HD.name), extractRawId(ids, HD.name)),
+                                vizierClient.getObjectDetails(vizierQueries.queries.getValue(HR.name), extractRawId(ids, HR.name)),
+                                vizierClient.getObjectDetails(vizierQueries.queries.getValue(GC.name), extractRawId(ids, GC.name))
+                        )
+                                .map {
+                                    listOf(
+                                            StarDetails.fromMap(SAO.name, it.t1),
+                                            StarDetails.fromMap(HIP.name, it.t2),
+                                            StarDetails.fromMap(TYC.name, it.t3),
+                                            StarDetails.fromMap(HD.name, it.t4),
+                                            StarDetails.fromMap(HR.name, it.t5),
+                                            StarDetails.fromMap(GC.name, it.t6)
+                                    )
+                                }
+                    }
 
     fun getStarsData(objectName: String) =
             simbadClient.getAllIdentifiers(objectName)
