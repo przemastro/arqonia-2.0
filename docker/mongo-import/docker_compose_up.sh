965e65f8e0f96ffcb4e0506aea44be1989c3b215
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-03-18 22:36:05 +0100

    Passwords in YAML spring -> now are encrypted and secured with 'keystore'

--- docker/mongo-import/docker_compose_up.sh
+++ docker/mongo-import/docker_compose_up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp -r ../src/main/resources/passwords/spring/encryption/key/ spring-encrypt-key/
 cp -r ../build/libs/ jar/
 cp -r ../src/main/resources/mongo/dump/ mongo-import/dump/
 docker-compose --verbose -f docker-compose-unix.yml up
