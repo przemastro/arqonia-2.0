3de05b5594b765a26a517ea75fd357ac4582186f
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-04-21 21:38:09 +0200

    added dedicated sh scripts for installing on production server

---
+++ installation/prod/step_3_run_docker_containers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ../../docker
+
+mkdir mongo-password spring-encrypt-key jar mongo-import/dump
+
+cp -r ../src/main/resources/passwords/docker/secrets/* mongo-password/
+cp -r ../src/main/resources/passwords/spring/encryption/key/* spring-encrypt-key/
+cp -r ../build/libs/* jar/
+cp -r ../src/main/resources/mongo/dump/* mongo-import/dump/
+
+docker-compose --verbose -f docker-compose-unix.yml up
