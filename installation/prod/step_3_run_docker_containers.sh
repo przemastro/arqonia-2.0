bc397b9bdf855fe12fb2ae053dbfbafafe1d5990
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-04-22 22:59:11 +0200

    Fixes (2)

--- installation/prod/step_3_run_docker_containers.sh
+++ installation/prod/step_3_run_docker_containers.sh
@@ -9,4 +9,4 @@ cp -r ../src/main/resources/passwords/spring/encryption/key/* spring-encrypt-key
 cp -r ../build/libs/* jar/
 cp -r ../src/main/resources/mongo/dump/* mongo-import/dump/
 
-docker-compose --verbose -f docker-compose-unix.yml up
+docker-compose -d --verbose -f docker-compose-unix.yml up
