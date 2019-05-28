574bb067af50f4676743c8e329aa016790a1543d
Adam Bekalarz <adam.bekalarz@allegro.pl>
2019-05-28 16:24:28 +0200

    Added new installation files (for dev and local)

--- installation/local_dev/step_3_run_docker_containers.sh
+++ installation/local_dev/step_3_run_docker_containers.sh
@@ -6,11 +6,11 @@ rm -rf jar/
 mkdir mongo-password spring-encrypt-key jar mongo-import/dump environment
 
 # SET UP CORRECT ENVIRONMENT VERSION (PROD/TEST/DEV and so on...)
-echo prod > environment/environment_name.txt
+echo local > environment/environment_name.txt
 
 cp -r ../src/main/resources/passwords/docker/secrets/* mongo-password/
 cp -r ../src/main/resources/passwords/spring/encryption/key/* spring-encrypt-key/
 cp -r ../build/libs/* jar/
 cp -r ../src/main/resources/mongo/dump/* mongo-import/dump/
 
-docker-compose --verbose -f docker-compose-unix.yml up -d
+docker-compose --verbose -f docker-compose-unix.yml up
